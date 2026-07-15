7bda0bf90e40ede303f3e7824b58578df7ef1cda
ramjananshari241 <ramjananshari241@gmail.com>
2026-07-15 13:14:08 +0800

    fix: preserve empty nav icons

--- src/components/nav/NavIcon.tsx
+++ src/components/nav/NavIcon.tsx
@@ -5,6 +5,21 @@ import { classNames, isValidUrl } from '@/src/lib/util'
 import { ApiColor } from '@/src/types/notion'
 import { useEffect, useState } from 'react'
 
+const getLocalIconPath = (icon: string, iconPath: string) => {
+  if (!icon) return ''
+
+  const malformedLocalSvgUrl = icon.match(/^https?:\/\/([^/]+\.svg)$/i)
+  if (malformedLocalSvgUrl?.[1]) {
+    return `${iconPath}/${malformedLocalSvgUrl[1]}`
+  }
+
+  if (icon.startsWith('/')) {
+    return /^\/[^/]+$/.test(icon) ? `${iconPath}${icon}` : icon
+  }
+
+  return `${iconPath}/${icon}`
+}
+
 export const NavIcon = ({
   icon,
   alt,
@@ -18,48 +33,39 @@ export const NavIcon = ({
   const [remoteFailed, setRemoteFailed] = useState(false)
 
   const safeIcon = (icon || '').trim()
-  const isUrl = isValidUrl(safeIcon)
+  const isMalformedLocalSvgUrl = /^https?:\/\/[^/]+\.svg$/i.test(safeIcon)
+  const isUrl = isValidUrl(safeIcon) && !isMalformedLocalSvgUrl
   const shouldUseRemoteImage = isUrl && !remoteFailed
-  const fallbackIconPath = `${ICON_PATH}/default.svg`
   const iconPath = shouldUseRemoteImage
     ? safeIcon
     : isUrl
-    ? fallbackIconPath
-    : safeIcon
-    ? safeIcon.startsWith('/')
-      ? /^\/[^/]+$/.test(safeIcon)
-        ? `${ICON_PATH}${safeIcon}`
-        : safeIcon
-      : `${ICON_PATH}/${safeIcon}`
-    : fallbackIconPath
+    ? ''
+    : getLocalIconPath(safeIcon, ICON_PATH)
 
   useEffect(() => {
     setRemoteFailed(false)
   }, [safeIcon])
 
-  return (
-    <>
-      {iconPath &&
-        (shouldUseRemoteImage ? (
-          <img
-            src={iconPath}
-            alt={alt}
-            className="h-full text-white text-opacity-0 aspect-square dark:text-black dark:text-opacity-0"
-            onError={() => setRemoteFailed(true)}
-          />
-        ) : (
-          <span
-            aria-label={alt}
-            className={classNames(
-              colorMap[(color + '_background') as ApiColor],
-              'aspect-square h-full text-transparent'
-            )}
-            style={{
-              mask: `url(${iconPath}) no-repeat center`,
-              WebkitMask: `url(${iconPath}) no-repeat center`,
-            }}
-          />
-        ))}
-    </>
+  if (!iconPath) return null
+
+  return shouldUseRemoteImage ? (
+    <img
+      src={iconPath}
+      alt={alt}
+      className="h-full text-white text-opacity-0 aspect-square dark:text-black dark:text-opacity-0"
+      onError={() => setRemoteFailed(true)}
+    />
+  ) : (
+    <span
+      aria-label={alt}
+      className={classNames(
+        colorMap[(color + '_background') as ApiColor],
+        'aspect-square h-full text-transparent'
+      )}
+      style={{
+        mask: `url(${iconPath}) no-repeat center`,
+        WebkitMask: `url(${iconPath}) no-repeat center`,
+      }}
+    />
   )
 }
